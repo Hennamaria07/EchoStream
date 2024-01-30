574f54509190424c7b270ed4220594aec2d665cb
Henna Maria <hennamaria2001@gmail.com>
2024-01-30 13:58:37 +0530

    utils code updated

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const databaseConnection = async () => {
+const databaseConnection = () => {
     mongoose.connect(`${process.env.DATABASE_URL}${DB_NAME}`)
     .then((res) => console.log(`database is connected with ${res.connection.host}`))
     .catch((err) => console.log(err.message));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv';
 import databaseConnection from "./db/index.js";
+import app from './app.js';
 dotenv.config(
     {path: './env'}
     );
-
 databaseConnection();
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running at port: ${process.env.PORT}`);
+});
