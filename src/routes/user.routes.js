f58ae0b8956accc214093a54fa2eac0d29502581
Henna Maria <hennamaria2001@gmail.com>
2024-02-03 13:18:53 +0530

    user profile route were added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changeCurrentPassword, currentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccount, updateAvatar, updateCoverImg } from "../controllers/user.controllers.js";
+import { changeCurrentPassword, currentUser, getUserChannelProfile, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccount, updateAvatar, updateCoverImg } from "../controllers/user.controllers.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyToken } from "../middleware/auth.middleware.js";
 
@@ -12,8 +12,9 @@ router.route("/logout").post(verifyToken, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/update-password").post(verifyToken, changeCurrentPassword);
 router.route("/update-account").post(verifyToken, updateAccount);
-router.route("/update-avatar").post(verifyToken, upload.single("avatar"), updateAvatar);
-router.route("/update-coverimg").post(verifyToken, upload.single("coverImg"), updateCoverImg);
+router.route("/update-avatar").patch(verifyToken, upload.single("avatar"), updateAvatar);
+router.route("/update-coverimg").patch(verifyToken, upload.single("coverImg"), updateCoverImg);
 router.route("/current-user").get(verifyToken, currentUser)
+router.route("/:username").get(verifyToken, getUserChannelProfile)
 
 export default router;
